820b48cd52b36f01076a89668295adc18549335b
tride <tride@DESKTOP-JCBSPBJ>
2020-05-09 17:22:20 +0300

    top-menu fix

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,7 +1,28 @@
 "use strict";
 
-function toggleClassToActive() {
+document.querySelector('#categories').onclick = function () {
   this.classList.toggle('active');
-}
+};
 
-document.querySelector('#categories').onclick = toggleClassToActive;
+document.querySelector('.mobile-menu').onclick = function () {
+  var menu = document.querySelector('.menu');
+  this.classList.toggle('active');
+  menu.classList.toggle('active');
+};
+
+document.querySelector('.has-children').onclick = function () {
+  this.classList.toggle('open');
+};
+
+document.onclick = function (e) {
+  var currentLiElement = document.querySelector('.has-children.active');
+  var currentLink = document.querySelector('.has-children.active a');
+
+  if (currentLiElement.classList.contains('open') && e.target !== currentLiElement) {
+    currentLiElement.classList.toggle('open');
+  }
+
+  if (e.target === currentLink) {
+    currentLiElement.classList.toggle('open');
+  }
+};
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,28 @@
-function toggleClassToActive () {
+document.querySelector('#categories').onclick = function () {
     this.classList.toggle('active');
-}
+};
 
-document.querySelector('#categories').onclick = toggleClassToActive;
+document.querySelector('.mobile-menu').onclick = function () {
+    const menu = document.querySelector('.menu');
 
+    this.classList.toggle('active');
+    menu.classList.toggle('active');
+};
 
+document.querySelector('.has-children').onclick = function () {
+    this.classList.toggle('open');
+};
+
+document.onclick = function (e) {
+    const currentLiElement = document.querySelector('.has-children.active');
+    const currentLink = document.querySelector('.has-children.active a');
+
+    if (currentLiElement.classList.contains('open') && e.target !== currentLiElement) {
+        currentLiElement.classList.toggle('open');
+    }
+
+    if(e.target === currentLink) {
+        currentLiElement.classList.toggle('open');
+    }
+
+};
