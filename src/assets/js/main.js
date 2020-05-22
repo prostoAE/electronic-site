963bdcd9e27b1ec927840cc6df1c09af43475aed
tride <tride@DESKTOP-JCBSPBJ>
2020-05-22 10:13:41 +0300

    count time offer ready

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -9,8 +9,8 @@ function mainSliderInit() {
         speed: 1000,
         slidesToShow: 1,
         slidesToScroll: 1,
-        // fade: true,
-        // cssEase: 'linear',
+        fade: true,
+        cssEase: 'linear',
         prevArrow: '<button type="button" class="slick-prev"></button>',
         nextArrow: '<button type="button" class="slick-next"></button>',
     });
@@ -121,6 +121,26 @@ function setProductCount() {
     }
 }
 
+// Count Time
+function makeTimer() {
+    var endTime = new Date("may  21, 2020 17:00:00");
+    var endTime = (Date.parse(endTime)) / 1000;
+    var now = new Date();
+    var now = (Date.parse(now) / 1000);
+    var timeLeft = endTime - now;
+    var days = Math.floor(timeLeft / 86400);
+    var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+    var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+    var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+    if (hours < "10") { hours = "0" + hours; }
+    if (minutes < "10") { minutes = "0" + minutes; }
+    if (seconds < "10") { seconds = "0" + seconds; }
+    $("#days").html(days);
+    $("#hour").html(hours);
+    $("#min").html(minutes);
+    $("#sec").html(seconds);
+}
+setInterval(function() { makeTimer(); }, 300);
 
 
 
